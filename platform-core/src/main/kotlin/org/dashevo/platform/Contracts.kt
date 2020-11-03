95d7e586c1bc8ea8a73a6fe1f197510fbadc48c5
HashEngineering <hashengineeringsolutions@gmail.com>
2020-11-02 18:55:11 -0800

    Contracts: Add broadcast, fix get to save retrieved contracts

--- platform-core/src/main/kotlin/org/dashevo/platform/Contracts.kt
+++ platform-core/src/main/kotlin/org/dashevo/platform/Contracts.kt
@@ -6,12 +6,23 @@
  */
 package org.dashevo.platform
 
+import org.bitcoinj.core.ECKey
+import org.dashevo.dpp.contract.DataContractCreateTransition
 import org.dashevo.dpp.contract.DataContract
 import org.dashevo.dpp.identifier.Identifier
 import org.dashevo.dpp.identity.Identity
 import java.util.*
 
 class Contracts(val platform: Platform) {
+
+    fun broadcast(dataContract: DataContract, identity: Identity, privateKey: ECKey, index: Int) : DataContractCreateTransition {
+        val  dataContractCreateTransition = platform.dpp.dataContract.createStateTransition(dataContract);
+
+        platform.broadcastStateTransition(dataContractCreateTransition, identity, privateKey, index)
+
+        return dataContractCreateTransition;
+    }
+
     fun create(documentDefinitions: MutableMap<String, Any?>, identity: Identity): DataContract {
         return platform.dpp.dataContract.createDataContract(identity.id.toBuffer(), documentDefinitions)
     }
@@ -37,11 +48,13 @@ class Contracts(val platform: Platform) {
             try {
                 val rawContract = platform.client.getDataContract(identifier.toBuffer()) ?: return null
 
-                val contract = platform.dpp.dataContract.createFromSerialized(rawContract.toByteArray())
+                val contract = platform.dpp.dataContract.createFromBuffer(rawContract.toByteArray())
                 val app = ContractInfo(contract.id, contract)
                 // If we do not have even the identifier in this.apps, we add it with timestamp as key
                 if (localContract == null) {
                     platform.apps[Date().toString()] = app
+                } else {
+                    localContract.dataContract = contract
                 }
                 return contract;
             } catch (e: Exception) {
