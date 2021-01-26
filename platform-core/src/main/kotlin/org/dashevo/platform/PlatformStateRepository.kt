1ce244930970a9beca198a6a731efe6a8f2faabb
HashEngineering <hashengineeringsolutions@gmail.com>
2021-01-25 20:54:51 -0800

    PlatformStateRepository: Add default functionality to most methods

--- platform-core/src/main/kotlin/org/dashevo/platform/PlatformStateRepository.kt
+++ platform-core/src/main/kotlin/org/dashevo/platform/PlatformStateRepository.kt
@@ -15,52 +15,76 @@ import org.dashevo.dpp.document.Document
 import org.dashevo.dpp.identifier.Identifier
 import org.dashevo.dpp.identity.Identity
 
-class PlatformStateRepository(val platform: Platform) : StateRepository {
+open class PlatformStateRepository(val platform: Platform) : StateRepository {
+    private val identityMap = hashMapOf<Identifier, Identity>()
+    private val identityHashesMap = hashMapOf<Identifier, List<ByteArray>>()
+    private val contractsMap = hashMapOf<Identifier, DataContract>()
+    private val outPointBufferSet = hashSetOf<ByteArray>()
+
     override fun checkAssetLockTransactionOutPointExists(outPointBuffer: ByteArray): Boolean {
-        TODO("Not yet implemented")
+        return outPointBufferSet.contains(outPointBuffer)
     }
 
     override fun fetchDataContract(id: Identifier): DataContract? {
+        if (contractsMap.containsKey(id))
+            return contractsMap[id]
+
         val contractInfo = platform.apps.values.find { it.contractId == id }
         if (contractInfo?.dataContract != null)
             return contractInfo.dataContract
-        return platform.contracts.get(id)
+
+        val contract = platform.contracts.get(id)
+        if (contract != null)
+            storeDataContract(contract)
+
+        return contract
     }
 
     override fun fetchDocuments(contractId: Identifier, type: String, where: Any): List<Document> {
         return platform.documents.get(contractId, type, where as DocumentQuery)
     }
 
-    override fun fetchTransaction(id: String): Transaction {
-        TODO()
+    override fun fetchTransaction(id: String): Transaction? {
+        val txData = platform.client.getTransaction(id)?: return null
+        return Transaction(null, txData.toByteArray())
     }
 
     override fun removeDocument(contractId: Identifier, type: String, id: Identifier) {
-        TODO("Not yet implemented")
+        // do nothing for now
     }
 
     override fun storeAssetLockTransactionOutPoint(outPointBuffer: ByteArray) {
-        TODO("Not yet implemented")
+        outPointBufferSet.add(outPointBuffer)
     }
 
     override fun storeDataContract(dataContract: DataContract) {
-        TODO("Not yet implemented")
+        if (!contractsMap.containsKey(dataContract.id))
+            contractsMap[dataContract.id] = dataContract
     }
 
     override fun storeDocument(document: Document) {
-        TODO("Not yet implemented")
+        // do nothing for now
     }
 
     override fun storeIdentity(identity: Identity) {
-        TODO("Not yet implemented")
+        if (!identityMap.containsKey(identity.id))
+            identityMap[identity.id]
     }
 
-    override fun storeIdentityPublicKeyHashes(identity: Identifier, publicKeyHashes: List<ByteArray>) {
-        TODO("Not yet implemented")
+    override fun storeIdentityPublicKeyHashes(identifier: Identifier, publicKeyHashes: List<ByteArray>) {
+        identityHashesMap[identifier] = publicKeyHashes
     }
 
     override fun fetchIdentity(id: Identifier): Identity? {
-        return platform.identities.get(id)
+        if (identityMap.containsKey(id))
+            return identityMap[id]
+
+        val identity = platform.identities.get(id)
+
+        if (identity != null) {
+            storeIdentity(identity)
+        }
+        return identity
     }
 
     override fun fetchLatestPlatformBlockHeader(): Block {
