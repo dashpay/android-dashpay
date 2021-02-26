f01487200d178ee0fd66b2da737e1ef4b93d958d
HashEngineering <hashengineeringsolutions@gmail.com>
2021-02-25 20:37:03 -0800

    Profile: Add equals

--- dashpay/src/main/kotlin/org/dashevo/dashpay/Profile.kt
+++ dashpay/src/main/kotlin/org/dashevo/dashpay/Profile.kt
@@ -15,4 +15,13 @@ class Profile(document: Document) : AbstractDocument(document) {
         get() = getFieldByteArray("avatarHash")
     val avatarFingerprint: ByteArray?
         get() = getFieldByteArray("avatarFingerprint")
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Profile
+
+        return document.equals(other.document)
+    }
 }
