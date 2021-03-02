2f09b8ac4098bd42cdda18406a1bb3e97336608d
HashEngineering <hashengineeringsolutions@gmail.com>
2021-03-01 21:46:08 -0800

    Create BlockchainIdentityTest
    * Verify recovery phrase/seed
    * Update profile test

---
+++ dashpay/src/test/kotlin/org/dashevo/dashpay/BlockchainIdentityTest.kt
@@ -0,0 +1,34 @@
+package org.dashevo.dashpay
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.Test
+import kotlin.random.Random
+
+class BlockchainIdentityTest : PlatformNetwork() {
+
+    val blockchainIdentity = BlockchainIdentity(platform, 0, wallet)
+
+    @Test
+    fun checkWalletSeedTest() {
+        assertEquals(seed.split(' '), blockchainIdentity.wallet!!.keyChainSeed.mnemonicCode)
+    }
+
+    @Test
+    fun updateProfileTest() {
+        val displayName = "Hello "+ Random.nextInt()
+
+        blockchainIdentity.recoverIdentity(wallet.blockchainIdentityKeyChain.watchingKey.pubKeyHash)
+
+        val currentProfile = blockchainIdentity.getProfileFromPlatform()!!
+
+        val updatedProfile = blockchainIdentity.updateProfile(displayName, null, null, null, null, null)
+
+        val retrievedProfile = blockchainIdentity.getProfileFromPlatform()!!
+
+        assertNotEquals(currentProfile, retrievedProfile)
+        assertEquals(updatedProfile, Profile(retrievedProfile))
+    }
+
+
+}
