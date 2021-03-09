582c49969deb6403899abde5f27f6711ce00b445
HashEngineering <hashengineeringsolutions@gmail.com>
2021-03-09 08:01:55 -0800

    Add WalletOptions class

---
+++ platform-core/src/main/kotlin/org/dashevo/client/WalletOptions.kt
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2020-present, Dash Core Group
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package org.dashevo.client
+
+data class WalletOptions(val mnemonic: String?,
+                         val creationTime: Long = 0)
