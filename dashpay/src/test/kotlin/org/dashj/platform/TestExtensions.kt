009113a534368f301401ab8df49aacd6c094591f
HashEngineering <hashengineeringsolutions@gmail.com>
2022-08-31 17:08:08 -0700

    tests: add TxMetaDataTests for tx_metadata document

---
+++ dashpay/src/test/kotlin/org/dashj/platform/TestExtensions.kt
@@ -0,0 +1,32 @@
+package org.dashj.platform.dashpay
+
+import org.dashj.platform.dpp.deepCompare
+import org.opentest4j.AssertionFailedError
+
+fun assertMapEquals(expected: Map<String, Any?>, actual: Map<String, Any?>) {
+    val result = expected.deepCompare(actual)
+    if (!result) {
+        throw AssertionFailedError(null, expected, actual)
+    }
+}
+
+fun assertMapEquals(expected: Map<String, Any?>, actual: Map<String, Any?>, message: String) {
+    val result = expected.deepCompare(actual)
+    if (!result) {
+        throw AssertionFailedError(message, expected, actual)
+    }
+}
+
+fun assertMapNotEquals(expected: Map<String, Any?>, actual: Map<String, Any?>) {
+    val result = expected.deepCompare(actual)
+    if (result) {
+        throw AssertionFailedError(null, expected, actual)
+    }
+}
+
+fun assertListEquals(expected: List<Any>, actual: List<Any>) {
+    val result = expected.deepCompare(actual)
+    if (!result) {
+        throw AssertionFailedError(null, expected, actual)
+    }
+}
